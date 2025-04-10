124e303bc08596985852de0b0b4e045e8c484288
noaft <nvantoan1203@gmail.com>
2025-04-10 10:48:57 +0700

    remove mixup

--- CustomDataset/main.py
+++ CustomDataset/main.py
@@ -289,7 +289,7 @@ def main(args):
 
     if args.mixup > 0.:
         # smoothing is handled with mixup label transform
-        criterion = SoftTargetCrossEntropy(weight=class_weights.cuda())
+        criterion = SoftTargetCrossEntropy()
     elif args.smoothing:
         criterion = LabelSmoothingCrossEntropy(smoothing=args.smoothing, weight=class_weights.cuda())
     else:
